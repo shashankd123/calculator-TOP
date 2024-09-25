02c704fbbe36624b735bf5577de8718c8d41b6ba
shashankd123 <71559885+shashankd123@users.noreply.github.com>
2024-09-25 16:53:23 +0530

    Added javascript

--- script.js
+++ script.js
@@ -1,43 +1,133 @@
-const currCon = document.querySelector("#curr-con");
-const prevCon = document.querySelector("#pre-con");
+let mainDisplay = document.querySelector("#main-display");
+let secondaryDisplay = document.querySelector("#secondary-display");
+const keyConatiner = document.querySelector("#key-container");
 
-const keyCont = document.querySelector("#key-container");
+let val1 = "";
+let val2 = "";
+let symbol = "";
 
-let currVal = "";
-let prevVal = "";
+let display = `${val1}${symbol}${val2}`;
 
-keyCont.addEventListener("click", (e) => {
-    const value = e.target.value;
-    if(value != undefined){        
-        //Do the operations with the inputs
-        operate(value);        
-    }
-})
+let temp1 = "";
+let temp2 = "";
+let tempSymbol = "";
 
-function operate(val){
+let secondary = `${temp1}${tempSymbol}${temp2}`;
 
-    currVal = currCon.textContent;
+keyConatiner.addEventListener("click", (e) => {
+	const value = e.target.value;
+	
+	if (value != undefined) {
+		handleValue(value);
+	}
+});
 
-    if("123456789".includes(val)){
-        currVal = ("" + currVal + val)
-        currCon.textContent = currVal;
-    }else if(currVal !== "" && val == "0"){
-        currVal += val;
-        currCon.textContent = currVal;
-    }else if(!(currVal.includes(".")) && val === "."){
-        currVal += val;
-        currCon.textContent = currVal;
-    }else if(val == "<" && currVal.length > 0){
-            currVal = currVal.substring(0, currVal.length -1);
-            currCon.textContent = currVal;
-    }else if(val === "AC" || "/%X-+=".includes(val)){
-        if(val == "AC"){
-            prevVal = "";
-            currVal = "";
-            currCon.textContent = currVal;
-            prevCon.textContent = currVal;
-        }else{
-            operateResult();
-        }
-    }
+function handleValue(value) {
+	let numbers = "0123456789";
+	let symbols = "/%x-+";
+
+	if (symbols.includes(value) && val1 == "" && val2 == "") {
+		return;
+	}
+
+	if (numbers.includes(value)) {
+		if (symbol == "") {
+			if (!(val1 == "" && value == "0")) {
+				val1 += value;
+			}
+		} else {
+			if (!(val2 == "" && value == "0")) {
+				val2 += value;
+			}
+		}
+	} else if (symbols.includes(value)) {
+		if (val1 != "" && val2 != "") {
+			calculate();
+			val2 = "";
+		}
+		symbol = value;
+	} else if (value == "=") {
+		calculate();
+		symbol = "";
+		val2 = "";
+	} else if (value == "AC") {
+		defaultView();
+	} else if (value == "<") {
+		if (val2 != "") {
+			val2 = val2.slice(0, -1);
+		} else if (symbol != "") {
+			symbol = "";
+		} else if (val1 != "") {
+			val1 = val1.slice(0, -1);
+		}
+	}
+	if (value == ".") {
+		if (symbol == "") {
+			if (val1 == "") {
+				val1 = "0.";
+			} else if (!val1.includes(".")) {
+				val1 += value;
+			}
+		} else {
+			if (val2 == "") {
+				val2 = "0.";
+			} else if (!val2.includes(".")) {
+				val2 += value;
+			}
+		}
+	}
+	
+	mainDisplay.textContent = `${val1}${symbol}${val2}`;
+	secondaryDisplay.textContent = `${temp1}${tempSymbol}${temp2}`;
+
+	mainDisplay.style.fontSize = "40px";
+	if(mainDisplay.textContent.length > 9){
+		mainDisplay.style.fontSize = "20px";
+	}
+
+
+}
+
+function calculate() {
+
+	updateSecondary();
+	
+	switch (symbol) {
+		case "/":
+			val1 = parseFloat(val1) / parseFloat(val2);
+			break;
+		case "x":
+			val1 = parseFloat(val1) * parseFloat(val2);
+			break;
+		case "-":
+			val1 = parseFloat(val1) - parseFloat(val2);
+			break;
+		case "+":
+			val1 = parseFloat(val1) + parseFloat(val2);
+			break;
+		case "%":
+			val1 = (parseFloat(val1) / 100) * parseFloat(val2);
+			break;
+	}
+}
+
+function updateSecondary(){
+	temp1 = val1;
+	tempSymbol = symbol;
+	temp2 = val2;
+}
+
+window.addEventListener("load", () => {
+	defaultView();
+});
+
+function defaultView() {
+	mainDisplay.textContent = "";
+	secondaryDisplay.textContent = "";
+	val1 = "";
+	val2 = "";
+	symbol = "";
+	display = `${val1}${symbol}${val2}`;
+	secondary = `${temp1}${tempSymbol}${temp2}`;
+	updateSecondary()
 }
